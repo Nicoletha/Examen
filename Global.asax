b5506c934fbc257c981595abf8e1c3c6a038eb86
Nicoletha <nicolgastelum5@gmail.com>
2024-10-24 09:38:57 -0700

    examen

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamenPractico__U2.MvcApplication" Language="C#" %>
---
+++ Models/Respuesta.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenPractico__U2.Models
+{
+    public class Respuesta
+    {
+        [JsonProperty("message")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+}
